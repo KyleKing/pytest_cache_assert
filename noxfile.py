9e90e2cfb33d546130f587cfb91fea23ec412a1a
Kyle King <KyleKing@users.noreply.github.com>
2021-10-30 13:08:32 -0400

    build: update calcipy and noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,8 @@
 """nox-poetry configuration file."""
 
-from calcipy.dev.noxfile import build_check, build_dist, check_safety, coverage, tests  # noqa: F401
+from calcipy.dev.noxfile import (  # noqa: F401
+    build_check, build_dist, check_safety, check_security, coverage, pin_dev_dependencies, tests,
+)
+
+# Ensure that non-calcipy dev-dependencies are available in Nox environments
+pin_dev_dependencies(['pydantic>=1.8.2', 'cerberus>=1.3.4'])

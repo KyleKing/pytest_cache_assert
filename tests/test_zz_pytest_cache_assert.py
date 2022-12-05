85ea7583a68bb83b617cfb71c3bbcec41beea173
Kyle King <KyleKing@users.noreply.github.com>
2022-12-04 21:16:56 -0500

    ci: update copier

--- tests/test_zz_pytest_cache_assert.py
+++ tests/test_zz_pytest_cache_assert.py
@@ -7,7 +7,7 @@ from pytest_cache_assert import __version__
 try:
     import tomllib
 except ModuleNotFoundError:
-    import tomli as tomllib  # ignore: type[no-redef]
+    import tomli as tomllib  # type: ignore[no-redef]
 
 
 def test_version():

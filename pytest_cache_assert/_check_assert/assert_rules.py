049ac3010543a6f3e481f07eb856dfa567842ce4
Kyle King <KyleKing@users.noreply.github.com>
2023-01-10 20:07:43 -0500

    ci: correct type check comment

--- pytest_cache_assert/_check_assert/assert_rules.py
+++ pytest_cache_assert/_check_assert/assert_rules.py
@@ -160,7 +160,7 @@ def _check_date_proximity(
         return (new_date - old_date) <= time_delta
     elif comparator == Comparator.GTE:
         return (new_date - old_date) >= time_delta
-    return abs(new_date - old_date) <= time_delta  # mypy: ignore[arg-type]
+    return abs(new_date - old_date) <= time_delta  # type: ignore[arg-type]
 
 
 @beartype

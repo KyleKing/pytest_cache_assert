ab3345c3108a091d877b9a8a6784cc3998a56452
Kyle King <KyleKing@users.noreply.github.com>
2023-07-23 08:14:37 -0400

    build: copier-auto-update

--- pytest_cache_assert/__init__.py
+++ pytest_cache_assert/__init__.py
@@ -1,7 +1,59 @@
 """pytest_cache_assert."""
 
-from ._check_assert.assert_config import AssertConfig  # noqa: F401
-from ._check_assert.assert_rules import (  # noqa: F401
+from enum import Enum
+from os import getenv
+
+from beartype import BeartypeConf
+from beartype.claw import beartype_this_package
+from typing_extensions import Self  # noqa: UP035
+
+__version__ = '3.0.8'
+__pkg_name__ = 'pytest_cache_assert'
+
+
+class _BeartypeModes(Enum):
+    """Supported global beartype modes."""
+
+    ERROR = 'ERROR'
+    WARNING = 'WARNING'
+    OFF = None
+
+    @classmethod
+    def from_environment(cls) -> Self:
+        """Return the configured mode."""
+        beartype_mode = getenv('BEARTYPE_MODE') or None
+        try:
+            return cls(beartype_mode)
+        except ValueError:
+            msg = f"'BEARTYPE_MODE={beartype_mode}' is not an allowed mode from {[_e.value for _e in cls]}"
+            raise ValueError(
+                msg,
+            ) from None
+
+
+def configure_beartype() -> None:
+    """Optionally configure beartype globally."""
+    beartype_mode = _BeartypeModes.from_environment()
+
+    if beartype_mode != _BeartypeModes.OFF:
+        # PLANNED: Appease mypy and pyright, but this is a private import
+        from beartype.roar._roarwarn import _BeartypeConfReduceDecoratorExceptionToWarningDefault
+        beartype_warning_default = _BeartypeConfReduceDecoratorExceptionToWarningDefault
+
+        beartype_this_package(conf=BeartypeConf(
+            warning_cls_on_decorator_exception=(
+                None if beartype_mode == _BeartypeModes.ERROR else beartype_warning_default
+            ),
+            is_color=getenv('BEARTYPE_NO_COLOR') is not None),
+        )
+
+
+configure_beartype()
+
+# ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
+
+from ._check_assert.assert_config import AssertConfig  # noqa: E402,F401
+from ._check_assert.assert_rules import (  # noqa: E402,F401
     AssertRule,
     Comparator,
     Wild,
@@ -11,13 +63,8 @@ from ._check_assert.assert_rules import (  # noqa: F401
     gen_check_date_proximity,
     gen_check_date_range,
 )
-from ._check_assert.cache_store import CacheStoreType, LocalJSONCacheStore  # noqa: F401
-from ._check_assert.config import CacheAssertContainerKeys, register, retrieve  # noqa: F401
-from ._check_assert.converter import Converter  # noqa: F401
-from ._check_assert.error_message import NoCacheError  # noqa: F401
-from ._check_assert.validator import DictDiffValidator, ValidatorType  # noqa: F401
-
-__version__ = '3.0.8'
-__pkg_name__ = 'pytest_cache_assert'
-
-# ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
+from ._check_assert.cache_store import CacheStoreType, LocalJSONCacheStore  # noqa: E402,F401
+from ._check_assert.config import CacheAssertContainerKeys, register, retrieve  # noqa: E402,F401
+from ._check_assert.converter import Converter  # noqa: E402,F401
+from ._check_assert.error_message import NoCacheError  # noqa: E402,F401
+from ._check_assert.validator import DictDiffValidator, ValidatorType  # noqa: E402,F401
